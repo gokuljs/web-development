d60d07451c6fa273c63b0bb148352be7ef075285
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-01-31 22:53:46 +0530

    nested arrays and objects very important

--- javascript/object.js
+++ javascript/object.js
@@ -6,3 +6,23 @@ var person = {
 console.log(person.age);
 console.log(person.name);
 console.log(person.city);
+
+
+var crazy = [{
+    name: "gokuljs",
+    age: 19,
+    job: "developer"
+}, {
+    name: "gokuljs",
+    age: 19,
+    job: "developer"
+
+}, {
+    name: "gokuljs",
+    age: 19,
+    job: "developer"
+
+}]
+
+console.log(crazy[1].name);
+console.log(crazy.length)
