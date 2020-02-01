64465536ba913d9beeb6ed36c2fe9678d56779bd
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-01 18:32:37 +0530

    dom manipulation

---
+++ DOM/dom1.js
@@ -0,0 +1,13 @@
+// two main parts in java script 
+// selector
+// manipulation
+// selector 
+var tags = document.getElementsByClassName("bolded"); // getting element by class name
+console.log(tags.lenght);
+console.log(tags[0]);
+console.log(tags);
+
+// getting elelement by tag name
+var tag = document.getElementsByTagName("li");
+
+console.log(tag);
