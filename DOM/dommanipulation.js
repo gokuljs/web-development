2755cba48949765c05d6091ba60ba001aaa1fc81
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-04 20:41:42 +0530

    dom manipulation

--- DOM/dommanipulation.js
+++ DOM/dommanipulation.js
@@ -1,3 +1,16 @@
 // dom manipulation
 // involves changing element styles , adding and removing classes, chaingin content of the tag
 // changing attributes
+// style is the one way to manipulate the ellement style 
+
+
+// var h1 = document.querySelector("h1");
+// h1.classList.add("big");
+
+// classlist is the other method which is used for dom manipulation
+
+// var h1 = document.querySelector("h1");
+// h1.classList.add("big");
+
+// toggel will add the class if no class is assigned 
+// toogel wil remove the class if it is already present
