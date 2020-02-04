8bffabc64cc361e72f6e692407130dcbac2b3c4a
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-04 20:57:45 +0530

    dom manipulation

--- DOM/dommanipulation.js
+++ DOM/dommanipulation.js
@@ -14,3 +14,9 @@
 
 // toggel will add the class if no class is assigned 
 // toogel wil remove the class if it is already present
+//classlist is not an array 
+
+// var p=document.getElementsByTagName("p")[0];
+//p.textContent 
+// so textcontent displays everything inside the web page 
+// and textcontent can used to change the content inside the paragraph
