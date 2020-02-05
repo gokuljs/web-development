e99076eb8ad2b34e7865c95aea967003942c3158
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-05 13:14:59 +0530

    dom manipulation

--- DOM/dommanipulation.js
+++ DOM/dommanipulation.js
@@ -23,3 +23,10 @@
 
 //var ul=document.querySelector("ul");
 // ul.textContent
+
+// getting started with changing attributes 
+// use get attribute and set attribute to read and write attributes like src and href
+// get attribute is going to get the particular attribute
+// var img1=document.getElementsByTagName("img")[0];
+// img1.getAttribute("src"); output will be the src of that image 
+// img1.setattribute("attribute_name","wat u have to change it to ");
