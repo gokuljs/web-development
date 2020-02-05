f689608c72e9827f1e42e0795977ec52f8ab3639
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-05 15:00:02 +0530

    dom events

--- DOM/domevent.js
+++ DOM/domevent.js
@@ -1,2 +1,16 @@
 // dom events are used to make things interactive
 //like clicking on a button,howering a link,dragging and dropping a link,pressing the enter key
+// process is select an element add a event listner
+// to add listen we uses add event listener
+//syntax
+//element.addEventListener(type,functionTocall);
+//var h1=document.querySelector("h1");
+//h1.addEventListener("click",function(){
+//alert("h1 was clicked");
+//});
+
+/*h1.addEventListener("click",function(){
+    h1.style.color="orange";
+});
+
+*/
