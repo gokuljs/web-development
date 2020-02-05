dacf96d3b09027e44ba162ab884f3dff7adbba0e
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-06 02:15:18 +0530

    origin master

--- DOM/scorekeeper.js
+++ DOM/scorekeeper.js
@@ -3,6 +3,8 @@ var p2 = document.getElementById("player2");
 var dis1 = document.querySelector("#p1display");
 var dis2 = document.querySelector("#p2display");
 var reset = document.querySelector("#reset");
+var numinput = document.querySelector("input");
+var p3 = document.querySelector("#p3"); // or if remove the id tag ang p space span tag means the tag present inside the paragraph tag
 var p1score = 0;
 var p2score = 0;
 var gameover = false;
@@ -12,7 +14,7 @@ p1.addEventListener("click", function() {
     if (!gameover) {
         p1score++; //p1score clicked;
         if (p1score === winning) {
-            console.log("you won");
+            gameover = true;
         }
         dis1.textContent = p1score;
     }
@@ -24,7 +26,7 @@ p2.addEventListener("click", function() {
 
         p2score++;
         if (p2score === winning) {
-            console.log("you won");
+            gameover = true;
         }
 
         dis2.textContent = p2score;
@@ -36,5 +38,13 @@ reset.addEventListener("click", function() {
     p1score = 0;
     dis2.textContent = p2score;
     dis1.textContent = p1score;
+
+});
+
+numinput.addEventListener("change", function() {
+
+    // change is an other kind of an event the takes place when any kind of change takes place
+
+    p3.textContent = numinput.value; // it gives the string of a value 
 
 });
