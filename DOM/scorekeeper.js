b2386a321ab046a1fb0a981b7fc44bb114392348
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-06 01:55:56 +0530

    origin master

--- DOM/scorekeeper.js
+++ DOM/scorekeeper.js
@@ -1,10 +1,26 @@
 var p1 = document.querySelector("#player1");
 var p2 = document.getElementById("player2");
-var h1 = document.querySelector("h1");
+var dis1 = document.querySelector("#p1display");
+var dis2 = document.querySelector("#p2display");
+var reset = document.querySelector("#reset");
 var p1score = 0;
+var p2score = 0;
 
 p1.addEventListener("click", function() {
     p1score++; //p1score clicked;
-    h1.textContent = p1score;
+    dis1.textContent = p1score;
+
+});
+
+p2.addEventListener("click", function() {
+    p2score++;
+    dis2.textContent = p2score;
+});
+
+reset.addEventListener("click", function() {
+    p2score = 0;
+    p1score = 0;
+    dis2.textContent = p2score;
+    dis1.textContent = p1score;
 
 });
