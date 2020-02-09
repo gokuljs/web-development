c909a4b0a21eb825cfdc9572c70ee49f99089979
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-09 12:39:33 +0530

    origin master

--- color game project/rgb.js
+++ color game project/rgb.js
@@ -9,10 +9,39 @@ var colors = [
 
 // spaces between the numbers are important
 var squares = document.querySelectorAll(".square");
-
+var pickedcolor = pickcolor();
+var test1 = document.getElementById("test1")
+test1.textContent = pickedcolor;
+var messagedisplay = document.getElementById("message");
 for (var i = 0; i < squares.length; i++) {
     // add intial colors to all squares 
     squares[i].style.backgroundColor = colors[i];
     // backgroundColor work for firefox
+    // add click listener to squares
+    squares[i].addEventListener("click", function() {
 
+        // compare color to the picked color 
+        var clicked = this.style.backgroundColor;
+        if (clicked === pickedcolor) {
+            messagedisplay.textContent = "correct"
+            changecolors(clicked);
+
+
+        } else {
+            this.style.backgroundColor = "#232323"
+            messagedisplay.textContent = "incorrect";
+        }
+    });
+
+}
+
+function changecolors(color) {
+    for (var i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickcolor() {
+    var random = Math.floor(Math.random() * colors.length + 1); // we are picking random color 
+    return colors[random];
 }
