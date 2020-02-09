f211ad66bca6f2d5027383ac130c24f23da0ffcd
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-09 22:32:24 +0530

    rgb.js

--- color game project/rgb.js
+++ color game project/rgb.js
@@ -1,11 +1,4 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 // spaces between the numbers are important
 var squares = document.querySelectorAll(".square");
@@ -42,6 +35,30 @@ function changecolors(color) {
 }
 
 function pickcolor() {
-    var random = Math.floor(Math.random() * colors.length + 1); // we are picking random color 
+    var random = Math.floor(Math.random() * colors.length); // we are picking random color
     return colors[random];
+}
+
+// function for generating random colors 
+function generateRandomColors(num) {
+    // make an random color
+    var arr = [];
+    // repaet num times
+    for (var i = 0; i < num; i++) {
+        // get random colors pushed into the array 
+        arr.push(randomcolor());
+    }
+    return arr;
+}
+
+function randomcolor() {
+    // we have rgb red from 0 to 255;
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    // we have rgb blue from 0 to 255;
+    // we have rgb green from 0 to 255;
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+
+
 }
