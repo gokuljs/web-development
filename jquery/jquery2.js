ef663bb01c40c702f30786c9e8e0ecabb3dc5a19
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-21 19:24:00 +0530

    jquery.html

---
+++ jquery/jquery2.js
@@ -0,0 +1,7 @@
+// selectls all the div and give them purple background
+
+$("div").css("background", "purple");
+
+$(".highlight").css("width", "200px");
+
+$("#third").css("border", "2px solid orange");
