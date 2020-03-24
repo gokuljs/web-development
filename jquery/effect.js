cc3a89619277c16e7eb0481a303046b3309d5b22
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-24 19:24:38 +0530

    jquery simple animations

--- jquery/effect.js
+++ jquery/effect.js
@@ -1,3 +1,15 @@
-$("button").on('click', function() {
+$("#fadeout").on('click', function() {
     $("div").fadeOut(1000);
 });
+
+$("#s").on('click', function() {
+    $('div').slideUp();
+
+});
+
+
+$("#l").on('click', function() {
+    $('div').slideDown();
+
+
+}); // slide down works if the display is none in css
